aff09e3b86fd0e5427f8e2da9971b4030117e2a9
vcokltfre <vcokltfre@gmail.com>
2022-02-05 09:11:05 +0000

    feat: clean up and add docker compose

--- alembic/env.py
+++ alembic/env.py
@@ -6,25 +6,11 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from src.impl.database.metadata import metadata
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)  # type: ignore
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-
 target_metadata = metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 config.set_main_option("sqlalchemy.url", environ["DB_URI"])
 
